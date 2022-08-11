e15c8f5b59d6132dd1d314ec97fb5a83d027e063
Rufina Akhmadeeva <r.akhmadeeva@dodopizza.com>
2022-08-11 17:30:54 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tdd_kata_dodo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
