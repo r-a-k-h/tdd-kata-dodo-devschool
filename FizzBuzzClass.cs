c6d0732e818afd3c6e102ce5f741051a668cc7f5
Rufina Akhmadeeva <r.akhmadeeva@dodopizza.com>
2022-08-14 12:12:51 +0300

    Added test for input 1

---
+++ FizzBuzzClass.cs
@@ -0,0 +1,10 @@
+namespace tdd_kata_dodo
+{
+    public class FizzBuzzClass
+    {
+        public string FizzBuzz(int input)
+        {
+            return "1";
+        }
+    }
+}
--- tests/tdd-kata-tests/FizzBuzzTests.cs
+++ tests/tdd-kata-tests/FizzBuzzTests.cs
@@ -1,18 +1,19 @@
 using NUnit.Framework;
+using tdd_kata_dodo;
 
 namespace tdd_kata_tests
 {
     public class FizzBuzzTests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Test1()
+        [TestCase(1)]
+        public void For_other_input_return_input(int input)
         {
-            Assert.Pass();
+            var sut = new FizzBuzzClass();
+
+            var result = sut.FizzBuzz(input);
+
+            Assert.AreEqual("1", result);
         }
     }
 }
