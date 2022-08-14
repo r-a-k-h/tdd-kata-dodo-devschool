90a32ee0a483134698863a7819176198c035639b
Rufina Akhmadeeva <r.akhmadeeva@dodopizza.com>
2022-08-14 12:10:33 +0300

    Add tests project

---
+++ tests/tdd-kata-tests/FizzBuzzTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace tdd_kata_tests
+{
+    public class FizzBuzzTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
